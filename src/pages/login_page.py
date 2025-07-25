b98f1eacc43c51c82ffd87d0c28778432075715c
Benjamin Defays <d.benjamin@pvp.co.jp>
2025-07-25 14:08:00 +0900

    adds a test checking if a user blocked cannot log in. This test also checks if the error message appears and if this error message can be closed

--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -6,12 +6,20 @@ class LoginPage:
     USERNAME_FIELD = '//input[@id="user-name"]'
     PASSWORD_FIELD = '//input[@id="password"]'
     LOGIN_BUTTON = '//input[@id="login-button"]'
+    ERROR_MESSAGE = '//h3[@data-test="error"]'
+    CLOSE_ERROR_BUTTON = '//button[@class="error-button"]'
+    LOCKED_OUT_MSG = 'Epic sadface: Sorry, this user has been locked out.'
+    
     
     def __init__(self, page: Page):
         self.page = page
         
     async def login(self, username:str, password:str)->None:
-        """A single high-level async method to perform a complete login."""
+        """Perform a complete login."""
         await self.page.locator(selector=self.USERNAME_FIELD).fill(value=username)
         await self.page.locator(selector=self.PASSWORD_FIELD).fill(value=password)
         await self.page.locator(selector=self.LOGIN_BUTTON).click()
+        
+    async def close_error(self)->None:
+        """Close the error pop up """
+        await self.page.locator(selector=self.CLOSE_ERROR_BUTTON).click()
--- tests/e2e/test_login.py
+++ tests/e2e/test_login.py
@@ -8,6 +8,7 @@ import pytest
 class TestLogin:
     
     async def test_successful_login(self, page:Page):
+        """Test the login flow of a user that is not blocked or suspended"""
         config: ConfigReader = ConfigReader()
         loginPage:LoginPage = LoginPage(page=page)
         
@@ -16,9 +17,11 @@ class TestLogin:
         
         await loginPage.login(username=username, password=password)
         
+        #checks if the user arrives on the inventory page
         await expect(actual=page).to_have_url(url_or_reg_exp=config.get_inventory_url()) 
         
-    async def test_locked_users(self, page:Page):
+    async def test_locked_user(self, page:Page):
+        """Tests the login flow of a suspended user"""
         config: ConfigReader = ConfigReader()
         loginPage:LoginPage = LoginPage(page=page)
         
@@ -27,5 +30,13 @@ class TestLogin:
         
         await loginPage.login(username=username, password=password)
         
-        await expect(actual=page).to_have_url(url_or_reg_exp=config.get_website_url())        
+        #checks that the error message appears when the user logs in 
+        await expect(actual=page).to_have_url(url_or_reg_exp=config.get_website_url())
+        await expect(actual=page.locator(loginPage.ERROR_MESSAGE)).to_have_text(loginPage.LOCKED_OUT_MSG)
+        #Closes the error message and check that the message disappeared
+        await loginPage.close_error()
+        await expect(actual=page.locator(loginPage.ERROR_MESSAGE)).to_have_count(0)
+    
+        
+                
         
