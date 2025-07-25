9f6a3c9aa546e5bf6bb76f707143eb2e8f5ec050
Benjamin Defays <d.benjamin@pvp.co.jp>
2025-07-25 16:06:15 +0900

    adds more tests covering the login errors  errors + split the login page value between the xpath locators and the error message values

--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -2,24 +2,36 @@ from playwright.async_api import Page
 
 
 class LoginPage:
-    #xpath locator for the login page field
+    #xpath locators for the login page's fields
     USERNAME_FIELD = '//input[@id="user-name"]'
     PASSWORD_FIELD = '//input[@id="password"]'
     LOGIN_BUTTON = '//input[@id="login-button"]'
     ERROR_MESSAGE = '//h3[@data-test="error"]'
     CLOSE_ERROR_BUTTON = '//button[@class="error-button"]'
-    LOCKED_OUT_MSG = 'Epic sadface: Sorry, this user has been locked out.'
+    
+    #error messages used by this page
+    ERR_MSG_LOCK = 'Epic sadface: Sorry, this user has been locked out.'
+    ERR_MSG_WRNG = 'Epic sadface: Username and password do not match any user in this service'
+    ERR_MSG_PSWD = 'Epic sadface: Password is required'
+    ERR_MSG_USRN = 'Epic sadface: Username is required'
     
     
     def __init__(self, page: Page):
         self.page = page
         
-    async def login(self, username:str, password:str)->None:
+    async def login(self, username:str | None = None, password:str | None = None)->None:
         """Perform a complete login."""
-        await self.page.locator(selector=self.USERNAME_FIELD).fill(value=username)
-        await self.page.locator(selector=self.PASSWORD_FIELD).fill(value=password)
+        if username:
+            await self.page.locator(selector=self.USERNAME_FIELD).fill(value=username)
+        if password:
+            await self.page.locator(selector=self.PASSWORD_FIELD).fill(value=password)
         await self.page.locator(selector=self.LOGIN_BUTTON).click()
         
     async def close_error(self)->None:
         """Close the error pop up """
-        await self.page.locator(selector=self.CLOSE_ERROR_BUTTON).click()
+        await self.page.locator(selector=self.CLOSE_ERROR_BUTTON).click()    
+        
+    async def clear_login_fields(self)->None:
+        """clear the username and password fields on the login page"""
+        await self.page.locator(selector=self.USERNAME_FIELD).clear()
+        await self.page.locator(selector=self.PASSWORD_FIELD).clear()
--- tests/e2e/test_login.py
+++ tests/e2e/test_login.py
@@ -7,7 +7,7 @@ import pytest
 @pytest.mark.asyncio
 class TestLogin:
     
-    async def test_successful_login(self, page:Page):
+    async def test_successful_login(self, page:Page) -> None:
         """Test the login flow of a user that is not blocked or suspended"""
         config: ConfigReader = ConfigReader()
         loginPage:LoginPage = LoginPage(page=page)
@@ -20,7 +20,7 @@ class TestLogin:
         #checks if the user arrives on the inventory page
         await expect(actual=page).to_have_url(url_or_reg_exp=config.get_inventory_url()) 
         
-    async def test_locked_user(self, page:Page):
+    async def test_locked_user(self, page:Page) -> None:
         """Tests the login flow of a suspended user"""
         config: ConfigReader = ConfigReader()
         loginPage:LoginPage = LoginPage(page=page)
@@ -32,11 +32,37 @@ class TestLogin:
         
         #checks that the error message appears when the user logs in 
         await expect(actual=page).to_have_url(url_or_reg_exp=config.get_website_url())
-        await expect(actual=page.locator(loginPage.ERROR_MESSAGE)).to_have_text(loginPage.LOCKED_OUT_MSG)
+        await expect(actual=page.locator(loginPage.ERROR_MESSAGE)).to_have_text(loginPage.ERR_MSG_LOCK)
         #Closes the error message and check that the message disappeared
         await loginPage.close_error()
         await expect(actual=page.locator(loginPage.ERROR_MESSAGE)).to_have_count(0)
     
+    async def test_error_wrong_password_wrong_username(self, page:Page) -> None:
+        """Tests the error when a user tries to login with a wrong password and username"""
+        config: ConfigReader = ConfigReader()
+        loginPage:LoginPage = LoginPage(page=page)
+        emptyStr:str = " "
+        
+        await loginPage.login(username=emptyStr, password=emptyStr)
+        await expect(actual=page).to_have_url(url_or_reg_exp=config.get_website_url())
+        await expect(actual=page.locator(loginPage.ERROR_MESSAGE)).to_have_text(loginPage.ERR_MSG_WRNG)
+        
+    async def test_error_no_password(self, page:Page) -> None:
+        """Tests the error when a user tries to login without providing a password"""
+        config: ConfigReader = ConfigReader()
+        loginPage:LoginPage = LoginPage(page=page)
+        
+        await loginPage.login(username=config.get_standard_user())
+        await expect(actual=page).to_have_url(url_or_reg_exp=config.get_website_url())
+        await expect(actual=page.locator(loginPage.ERROR_MESSAGE)).to_have_text(loginPage.ERR_MSG_PSWD)
+    
+    async def test_error_no_username(self, page:Page) -> None:
+        """Tests the error when a user tries to login without providing a username"""
+        config: ConfigReader = ConfigReader()
+        loginPage:LoginPage = LoginPage(page=page)    
+        await loginPage.login(password=config.get_password())
+        await expect(actual=page).to_have_url(url_or_reg_exp=config.get_website_url())
+        await expect(actual=page.locator(loginPage.ERROR_MESSAGE)).to_have_text(loginPage.ERR_MSG_USRN)
         
                 
         
