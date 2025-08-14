61cbd8255bd9286020cd20cc5cfc03299371c70b
Benjamin Defays <d.benjamin@pvp.co.jp>
2025-08-14 17:13:04 +0900

    adapts the conftest page so each call of the browsercontext is made with a separate directory. this is better practice to not have the docker instance being locked if a test fails to destroy the page after it's done

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,18 +1,21 @@
 import pytest_asyncio
-import asyncio
 from playwright.async_api import async_playwright, BrowserContext, Page
 from src.utils.config_reader import ConfigReader
+import shutil
+import tempfile
 
 @pytest_asyncio.fixture
 async def page():
     config:ConfigReader = ConfigReader()
     base_url:str = config.get_website_url()
-    
+     # Create a temporary directory for the user profile
+    profile_dir = tempfile.mkdtemp()
     async with async_playwright() as p:
         """this is the base that will be executed before and after (from yield) each tests"""
         browser:BrowserContext = await p.chromium.launch_persistent_context(
-            user_data_dir="user_data",
-            headless=False,  # Run in headed mode to see what's happening
+            user_data_dir=profile_dir,
+            headless=True,  # Recommended for Docker/CI environments
+            args=["--disable-features=ProcessSingleton"] # A good safety measure
         )
         page:Page = await browser.new_page()
         await page.goto(base_url)
@@ -20,6 +23,5 @@ async def page():
         yield page 
         
         print("Closing browser.")
-        #give a bit of time to check the browser before the end of the test
-        await asyncio.sleep(2)
         await browser.close()
+        shutil.rmtree(profile_dir)
